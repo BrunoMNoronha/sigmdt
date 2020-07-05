e2a8335d9d6b0b3a431c84caf3654610d94332ad
Bruno M Noronha <bruno.developer@pm.me>
2020-07-05 11:45:41 -0300

    implementando controle de seriais

--- controllers/SerialController.js
+++ controllers/SerialController.js
@@ -5,30 +5,23 @@ const path = require('path');
 const csv = require('fast-csv');
 
 
-async function gravarCSV(row) {
-  try {
-    await knex('seriais')
-      .insert(row);
-  } catch (error) {
-    console.log(error);
-  }
-  return res.redirect('/seriais');
-}
-
-
 module.exports = {
   async index(req, res) {
-    return res.render('seriais/seriais.html');
+    return res.render('seriais/seriais.html', { id: req.query.id_produto });
   },
   async store(req, res, next) {
 
-    // console.log(req.body, req.file);
+    console.log(req.body, req.file);
 
     try {
       fs.createReadStream(path.resolve(__dirname, '..', 'uploads', req.file.filename))
         .pipe(csv.parse({ headers: false }))
         .on('error', error => console.error(error))
-        .on('data', async row => { await knex('seriais').insert(row) })
+        .on('data', async row => {
+          await knex('seriais')
+            .insert({ produto_id: req.body.produto_id, numero: row });
+
+        })
         .on('end', rowCount => console.log(`Parsed ${rowCount} rows`));
     } catch (error) {
       console.error(error)
