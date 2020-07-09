bff55838dd921537a2056e05c9fe74884e849bb2
Bruno M Noronha <bruno.developer@pm.me>
2020-07-09 14:13:58 -0300

    financeiro: correções de filtros da tabela

--- public/javascripts/financeiro.js
+++ public/javascripts/financeiro.js
@@ -2,20 +2,45 @@ var dateControl = document.querySelectorAll('input[type="date"]');
 var todayDate = new Date().toISOString().slice(0, 10);
 dateControl.forEach(element => {
     element.value = todayDate;
+    element.setAttribute('max',todayDate)
 });
 
-function selectFilter() {
-    var selected = $("#selectMovimentacao option:selected").val();
-    var tr = document.querySelectorAll('#trMovimentacao');
+function dateFilter() {
+    var tr = document.querySelectorAll('#trMovimentacoes');
+    var selected = $("#selectMovimentacoes option:selected").val();
+    var inputDataMin = $('#dataMin').val();
+    var inputDataMax = $('#dataMax').val();
+    var dataMin = new Date(inputDataMin);
+    var dataMax = new Date(inputDataMax);
 
     tr.forEach(element => {
         element.style.display = "";
     });
 
     tr.forEach(element => {
-        var td = element.children[1].textContent
-        if (td != selected && (selected)) {
+        var elemetTdMovimentacao = element.children[1].textContent
+        var elemetTdData = element.children[0].textContent
+        var dtDataValue = new Date(elemetTdData);
+
+        if (((dtDataValue < dataMin) || (dtDataValue > dataMax)) || (elemetTdMovimentacao != selected && (selected))) {
             element.style.display = "none";
+
         }
     });
 }
+
+// function selectFilter() {
+//     var selected = $("#selectMovimentacoes option:selected").val();
+//     var tr = document.querySelectorAll('#trMovimentacoes');
+
+//     tr.forEach(element => {
+//         element.style.display = "";
+//     });
+
+//     tr.forEach(element => {
+//         var td = element.children[1].textContent
+//         if (td != selected && (selected)) {
+//             element.style.display = "none";
+//         }
+//     });
+// }
