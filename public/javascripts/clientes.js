49c5cdbb1116ccaea5c8ffa859ddf082f763e4a6
Bruno M Noronha <bruno.developer@pm.me>
2020-07-13 18:49:28 -0300

    desabilitando campos de endereço

--- public/javascripts/clientes.js
+++ public/javascripts/clientes.js
@@ -1,22 +1,28 @@
-const inputCep = document.querySelector('#inputCep');
+const inputComplemento = document.querySelector('#inputComplemento');
 const inputEndereco = document.querySelector('#inputEndereco');
 const inputBairro = document.querySelector('#inputBairro');
 const inputCidade = document.querySelector('#inputCidade');
 const inputEstado = document.querySelector('#inputEstado');
+const inputCep = document.querySelector('#inputCep');
 
 function cepPromise() {
-    const inputCepValue = document.querySelector('#inputCep').value;
-    cep(inputCepValue).then(cep => {
-        inputCep.classList.remove('is-invalid')
-        inputEndereco.value = cep.street;
-        inputBairro.value = cep.neighborhood;
-        inputCidade.value = cep.city;
-        inputEstado.value = cep.state;
-    }, () => {
-        inputCep.classList.add('is-invalid')
-        inputEndereco.value = '';
-        inputBairro.value = '';
-        inputCidade.value = '';
-        inputEstado.value = '';
-    })
+	const inputCepValue = document.querySelector('#inputCep').value;
+	cep(inputCepValue).then(cep => {
+		inputComplemento.removeAttribute('readonly', false);
+		inputEndereco.removeAttribute('readonly', false);
+		inputCep.classList.remove('is-invalid');
+		inputBairro.value = cep.neighborhood;
+		inputEndereco.value = cep.street;
+		inputEstado.value = cep.state;
+		inputCidade.value = cep.city;
+	}, () => {
+		inputComplemento.setAttribute('readonly', true);
+		inputEndereco.setAttribute('readonly', true);
+		inputCep.classList.add('is-invalid');
+		inputComplemento.value = '';
+		inputEndereco.value = '';
+		inputBairro.value = '';
+		inputCidade.value = '';
+		inputEstado.value = '';
+	})
 }
